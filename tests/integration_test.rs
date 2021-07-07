bc377200b0550e71d9daf0bebf8a1a2fde0e5318
Umran Hussein <umran.hussein@gmail.com>
2021-07-06 21:57:27 -0700

    made integration test more meaningful

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,17 +1,40 @@
 use serde::Serialize;
+use serde_json;
 use automato::statemachine;
 
 #[derive(Serialize, Clone, Copy)]
-struct SharedData {}
+struct TxData {
+    id: i32
+}
 
 #[derive(Serialize, Clone, Copy)]
 struct AssociatedData {}
 
 struct Log {}
-impl Observer for Log {}
+impl Observer for Log {
+    fn on_init<T:Serialize,U:Serialize>(&self, to: &str, data:Option<T> , state_data:Option<U>) ->Result<(),()> {
+        println!("initializing to {}", to);
+
+        if let Some(d) = data {
+            match serde_json::to_string(&d) {
+                Ok(s) => println!("{}", s),
+                Err(_) => return Err(())
+            };
+        };
+
+        if let Some(d) = state_data {
+            match serde_json::to_string(&d) {
+                Ok(s) => println!("{}", s),
+                Err(_) => return Err(())
+            };
+        };
+
+        Ok(())
+    }
+}
 
 statemachine! {
-    Tx: SharedData {
+    Tx: TxData {
         init Pending: AssociatedData {
             submit => Submitting,
             cancel => Cancelled
@@ -33,7 +56,7 @@ statemachine! {
 
 #[test]
 fn transitions() {
-    let tx = Tx::init(SharedData {}, AssociatedData {}, Log {}).unwrap();
+    let tx = Tx::init(TxData { id: 6 }, AssociatedData {}, Log {}).unwrap();
     let tx = tx.submit(AssociatedData{}).unwrap();
     tx.accept(AssociatedData{}).unwrap();
 }
